0fea05b3c0de5f41b210c2fd2cac0ea59373a911
aleksander kutsenkov <ligalizer8@gmail.com>
2016-08-11 09:52:27 -0400

    m

--- public/js/script.js
+++ public/js/script.js
@@ -3,28 +3,39 @@
 var questions = [{
     question: "A great past time for you consists of:",
     choices: ["Drinking alcholic beverages.", "Smoking.", "Shooting a gun.", "Eating."],
-    correctAnswer: 1
+    correctAnswer: 1,
+
+
 }, {
     question: "What's most important thing in society'?",
     choices: ["Sport", "Money", "Religion", "Power"],
-    correctAnswer: 2
+    correctAnswer: 2,
+
 }, {
     question: "Pick a morning dring to start your day?",
     choices: ["Vodka", "Beer", "Сofee/tea", "champagne"],
-    correctAnswer: 1
+    correctAnswer: 1,
+
 }, {
     question: "Given the choices, you`d prefer to eat?",
     choices: ["Taco's", "Steak", "Hamburger", "Pizza"],
-    correctAnswer: 0
+    correctAnswer: 0,
+
 }, {
     question: "You would prefer to live?",
     choices: ["In a forest.", "I don`t really care.", "By the beach.", "In Siberia"],
-    correctAnswer: 0
+    correctAnswer: 0,
+
 }];
 
 var currentQuestion = 0;
 var correctAnswers = 0;
 var quizOver = false;
+var Germany = 0;
+var France = 0;
+var Italian =0;
+
+
 
 
 $(document).ready(function(){
@@ -47,16 +58,15 @@ $(document).ready(function(){
 
                 if (value == questions[currentQuestion].correctAnswer) {
                     correctAnswers++;
-                }
 
+                }
                 currentQuestion++; // Since we have already displayed the first question on DOM ready
                 if (currentQuestion < questions.length) {
                     displayCurrentQuestion();
-                } else {
+                }
+
+                   else {
                     displayScore();
-                    //                    $(document).find(".nextButton").toggle();
-                    //                    $(document).find(".playAgainButton").toggle();
-                    // Change the text in the next button to ask if user wants to play again
                     $(document).find(".nextButton").text("Play Again?");
                     quizOver = true;
                 }
@@ -72,7 +82,7 @@ $(document).ready(function(){
 
 
 
-// This displays the current question AND the choices
+
 function displayCurrentQuestion() {
 
     console.log("question is displayd");
@@ -82,7 +92,6 @@ function displayCurrentQuestion() {
     var choiceList = $(document).find(".quizContainer > .choiceList");
     var numChoices = questions[currentQuestion].choices.length;
 
-    // Set the questionClass text to the current question
     $(questionClass).text(question);
 
     // Remove all current <li> elements (if any)
